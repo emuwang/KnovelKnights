85c3ac5c445970a5376ca88e9b8183eed479a3bd
Emmanuel Wang <ewang19@dons.usfca.edu>
2024-04-07 04:38:38 -0700

    login working again

--- knovel_knight/src/App.js
+++ knovel_knight/src/App.js
@@ -5,6 +5,7 @@ import Events from './pages/Events';
 import Books from './pages/Books';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import Login from "./pages/Login"
 import ReviewFiendishCodex from './pages/CatinTheHat';
 import { Route, Routes } from 'react-router-dom';
 import Board from './pages/components/Board';
@@ -23,6 +24,7 @@ function App() {
           <Route path="/leaderboard" element={<Leaderboard />} />
           <Route path="/events" element={<Events />} /> 
           <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
           <Route path='/CatintheHat' element={<CatinTheHat />} />
         </Routes>
         {/* <Register /> */}
